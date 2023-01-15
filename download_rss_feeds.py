0009403fd02fe4ffa674d8a870875073b0c63fb4
taras-sereda <taras.y.sereda@proton.me>
2023-01-15 19:11:43 +0200

    multiproc download rss feed

--- download_rss_feeds.py
+++ download_rss_feeds.py
@@ -1,9 +1,14 @@
+import argparse
 import asyncio
+import concurrent.futures
+import csv
+import os
 import random
 import time
 from pathlib import Path
 
 import aiohttp
+import requests
 import tqdm
 
 from podindex import SessionLocal
@@ -19,7 +24,7 @@ async def get_rss(sess, podcast):
     try:
         sleep_prob = random.random()
         if sleep_prob > 0.8:
-            await asyncio.sleep(random.random()/3)
+            await asyncio.sleep(random.random() / 3)
 
         async with sess.get(podcast.originalUrl) as resp:
             if resp.status != 200:
@@ -33,6 +38,24 @@ async def get_rss(sess, podcast):
             aiohttp.ClientConnectionError
     ) as e:
         print(podcast.originalUrl, str(e))
+
+
+def get_rss_sequentially(podcast):
+    output_path = data_root.joinpath(f'{podcast.podcastGuid}.xml')
+    try:
+        resp = requests.get(podcast.originalUrl)
+        if resp.status_code != 200:
+            return podcast.podcastGuid, False
+        text = resp.text
+        with open(output_path, 'w') as f:
+            f.write(text)
+
+    except (
+            Exception
+    ) as e:
+        return podcast.podcastGuid, False
+
+    return podcast.podcastGuid, True
 
 
 def filter_already_downloaded(podcasts):
@@ -54,12 +77,44 @@ async def main():
     size = 300
     async with aiohttp.ClientSession() as sess:
         for chunk_idx in range(0, len(podcasts), size):
-            tasks_list = [asyncio.create_task(get_rss(sess, podcast)) for podcast in podcasts[chunk_idx: chunk_idx + size]]
+            tasks_list = [asyncio.create_task(get_rss(sess, podcast)) for podcast in
+                          podcasts[chunk_idx: chunk_idx + size]]
             for t in tqdm.tqdm(tasks_list):
                 await t
 
 
+def main_multiproc():
+    db = SessionLocal()
+    podcasts = get_podcasts(db, -1)
+    print(f'Total amount of podcasts {len(podcasts)}')
+    podcasts = filter_already_downloaded(podcasts)
+    random.shuffle(podcasts)
+    print(f'After filtering, total amount of podcasts {len(podcasts)}')
+
+    fd = open('unable_to_download_feeds.csv', 'w+')
+    writer = csv.DictWriter(fd, ['GUID'])
+    writer.writeheader()
+
+    futures = []
+    with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count() * 2) as executor:
+        for podcast in podcasts:
+            futures.append(executor.submit(get_rss_sequentially, podcast))
+        for future in tqdm.tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
+            res = future.result()
+            pod_guid, status = res
+            if status is False:
+                writer.writerow({'GUID': pod_guid})
+
+    fd.close()
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--implementation', choices=['asyncio', 'multiprocessing'])
+    args = parser.parse_args()
     start = time.time()
-    asyncio.run(main())
+    if args.implementation == 'asyncio':
+        asyncio.run(main())
+    else:
+        main_multiproc()
     print(time.time() - start)
